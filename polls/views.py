4100bb41efbe79a6f28f7c76aeb57d4092960783
Cyrille YAO <cyaoyapi@gmail.com>
2018-11-21 15:01:32 +0000

    First view : The most simple view in django

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(resquest):
+	""" Homepage vto say welcome to visitor. """
+
+	return HttpResponse("Hello, welcome to our django website.\nWe're going to do a polls application.")

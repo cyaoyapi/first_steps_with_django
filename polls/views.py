28c8795cad6e70b1696d9a5fec6b1603b613cf53
Cyrille YAO <cyaoyapi@gmail.com>
2018-11-23 18:12:09 +0000

    Rewriting views with generic classes of views.

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,30 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
+class IndexView(generic.ListView):
 	""" 
 	List the recents questions. 
 	"""
 
-	lastest_questions_list = Question.objects.order_by('-pub_date')[:5]
-	
-	context = {
-		'lastest_questions_list':lastest_questions_list
-	}
-	return render(request, 'polls/index.html', context)
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_questions_list'
 
-def detail(request, question_id):
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
 	"""
 	Display a question and its different choices for voting. 
 	"""
 
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question':question})
+	model = Question
+	template_name = 'polls/detail.html'
 
 
 def vote(request, question_id):
@@ -45,10 +45,10 @@ def vote(request, question_id):
 		choice_selected.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
 
-def results(request, question_id):
+class ResultsView(generic.DetailView):
 	"""
 	Display a question and the results of votes
 	"""
 
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question':question})
+	model = Question
+	template_name = 'polls/results.html'

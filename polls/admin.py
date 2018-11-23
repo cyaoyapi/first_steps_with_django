2325e2721fcf0b342a876a2240c9263678e69b01
Cyrille YAO <cyaoyapi@gmail.com>
2018-11-23 23:56:45 +0000

    Displaying 'Choices' forms as sub-forms in 'Question' form in adminsitration site.

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+	"""
+	Using it to add Choice form as sub-form of Question form in administration site.
+	"""
+
+	model = Choice
+	extra = 3
+	
+
+
 class QuestionAdmin(admin.ModelAdmin):
 	"""
 	A model to customize the form of question in the administration site.
@@ -11,6 +21,7 @@ class QuestionAdmin(admin.ModelAdmin):
 		(None , {'fields': ['question_text']}),
 		('Date information' , {'fields': ['pub_date']}),
 	]
+	inlines = [ChoiceInline]
 
 # Register your models here.
 

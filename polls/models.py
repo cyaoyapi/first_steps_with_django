9816da497cee590865d3dc28bef3ceb57bb84376
Cyrille YAO <cyaoyapi@gmail.com>
2018-11-23 03:46:21 +0000

    Adding special methods __str__ and was_recently_published

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -10,13 +13,34 @@ class Question(models.Model):
     question_text = models.CharField("Question text", max_length=255)
     pub_date = models.DateTimeField("Date plublished")
 
+    def __str__(self):
+    	"""
+    	Special method for best representation of objects.
+    	"""
+
+    	return f"{self.question_text}" # using f-string (python3.7)
+
+    def was_published_recently(self):
+    	"""
+    	Return true if the question was published recently else false
+    	"""
+
+    	return self.pub_date >= timezone.now() - datetime.delta(days=1)
+
 
 class Choice(models.Model):
 	"""
-	Model representing a choice of answer to a survey question
+	Model representing a choice of answer to a survey question.
 	"""
 
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField("Choice text", max_length=255)
 	votes = models.IntegerField("Votes", default=0)
-		
+
+	def __str__(self):
+		"""
+    	Special method for best representation of objects.
+		"""
+
+		return f"{self.choice_text}"
+

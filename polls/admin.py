3c46ff3e7a107aec2299e5abf5e00cf6e828e1d7
Cyrille YAO <cyaoyapi@gmail.com>
2018-11-23 23:23:03 +0000

    Customization of question form in administration site.

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+	"""
+	A model to customize the form of question in the administration site.
+	"""
+
+	fields = ['pub_date','question_text']
+
 # Register your models here.
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)

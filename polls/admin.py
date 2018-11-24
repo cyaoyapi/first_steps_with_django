05146cffa417df00059987cf024c71ca484e6460
Cyrille YAO <cyaoyapi@gmail.com>
2018-11-24 00:08:01 +0000

    Customization of questions list displaying in administration site.

--- polls/admin.py
+++ polls/admin.py
@@ -16,7 +16,7 @@ class QuestionAdmin(admin.ModelAdmin):
 	"""
 	A model to customize the form of question in the administration site.
 	"""
-
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
 	fieldsets = [
 		(None , {'fields': ['question_text']}),
 		('Date information' , {'fields': ['pub_date']}),
